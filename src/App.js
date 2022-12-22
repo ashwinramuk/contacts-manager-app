4430ad413be2a1f215c52404defc15c92a3f4d8c
hirak-gitv2 <royhirakp@gmail.com>
2022-12-22 12:00:07 +0530

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-
+import TotalContact from './Component/TotalContact';
 function App() {
   return (
     <div className="App">
+      <TotalContact/>
     </div>
   );
 }
---
+++ src/Component/TotalContact/TotalContact.js
@@ -0,0 +1,16 @@
+// import {useState , useEffect} from "react"
+import SideBar from "./SideBar/SideBar";
+import PageHead from "./PageHead/PageHead";
+import ContactsData from "./ContactsData/ContactsData";
+const TotalContact = ()=>{
+
+    return (
+        <>
+        <SideBar/>
+        <PageHead/>
+        <ContactsData/>
+        
+        </>
+    )
+}
+export default TotalContact;
