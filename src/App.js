cc1545c231578db1d5e736683225fafd67cae3bc
jilanibasha <jilanibasha>
2022-12-22 12:49:40 +0530

    added basic routing structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
+// import './App.css';
 
+import LoginPage from './Component/LogIn/loginpage';
 function App() {
+
   return (
     <>
-      
+        <BrowserRouter>
+          <Routes>
+            <Route path='/login' element={<LoginPage/>}/>
+          </Routes>
+        </BrowserRouter>
     </>
   );
 }
